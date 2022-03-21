5c9ac0068be2091edf6aafd046ba2060c72e03b6
vivekparthiban <vivek.prodigy@gmail.com>
2022-03-21 13:38:00 +0800

    Movie card details click dynamic class

--- src/app/partials/movie-card/movie-card.component.ts
+++ src/app/partials/movie-card/movie-card.component.ts
@@ -11,15 +11,18 @@ export class MovieCardComponent implements OnInit {
 
   @Input()
   movieData!: Movie;
+  cardSize : string = '';
   constructor(
     private appService: AppService
   ) { }
 
   ngOnInit(): void {
+    this.cardSize = 'halfWidth';
     console.info('Movie Data = ', this.movieData);
   }
 
   showDetails(): void {
+    this.cardSize = 'fullWidth';
     console.info(this.movieData.imdbID)
     this.appService.getDetails(this.movieData.imdbID).subscribe((res: SearchResult) => {
       console.info('movie response = ', res);
